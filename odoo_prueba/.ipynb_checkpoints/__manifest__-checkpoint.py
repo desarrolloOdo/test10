aa52c6f4ec21b75b7d88d78c3e6f4d61d71bc62b
ivan <odootest725@gmail.com>
2022-09-21 16:42:25 +0000

    [ADD] mudulo creado

--- odoo_prueba/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_prueba/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Odoo Academy',
+    'name': 'Odoo prueba',
     
-    'summary':"""Academy app to manage Training""",
+    'summary':"""Modulo creado pruebas""",
     
     'description': """
         Academy Module to manage Training:
